11de2a72752105a2bd77b6debff7b73db3707d9d
John M. Knutson <john.knutson@novu.com>
2021-07-20 12:28:07 -0500

    working

--- windrose.go
+++ windrose.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"text/template"
 )
@@ -12,66 +14,76 @@ type Coordinates struct {
 	X, Y int
 }
 
-// Arrow is an arrow
+// Arrow represents an SVG line object with marker
 type Arrow struct {
-	Start, Finish    Coordinates
-	StartX, StartY   int
-	FinishX, FinishY int
-	Width            float32
+	Start, Finish Coordinates
+	Width         float64
 }
 
-// Canvas represent SVG canvas
-type Canvas struct {
-	Width, Height int
-}
-
-func main() {
+// GenWindrose takes an input angle and fills a byte buffer with SVG data
+func GenWindrose(angleDeg float64, svgWindroseBuf *bytes.Buffer) error {
 	svgBaseTmplData, err := ioutil.ReadFile("windrose_base.svg.tmpl")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	svgArrowTmplData, err := ioutil.ReadFile("windrose_arrow.svg.tmpl")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	svgArrowTmpl, err := template.New("arrow").Parse(string(svgArrowTmplData))
 	if err != nil {
-		panic(err)
+		return err
 	}
 	svgBaseTmpl, err := template.New("arrow").Parse(string(svgBaseTmplData))
 	if err != nil {
-		panic(err)
+		return err
 	}
-	/*
-		double angle_rad = (angle - 90) * (M_PI / 180.0);
-		fprintf(stderr, "angle supplied: %f\n", angle);
 
-		struct Coordinates c_start;
-		c_start.x = (CANVAS_SIZE / 2);
-		c_start.y = (CANVAS_SIZE / 2);
-		struct Coordinates c_finish;
-		c_finish.x = c_start.x + (cos(angle_rad) * ARROW_LENGTH);
-		c_finish.y = c_start.y + (sin(angle_rad) * ARROW_LENGTH);
-		struct Canvas canvas;
-		canvas.width = CANVAS_SIZE;
-		canvas.height = CANVAS_SIZE;
-	*/
+	var (
+		arrow         Arrow
+		start, finish Coordinates
+	)
+	// starting point is static
+	start.X = 307 // not sure why this is offset by 7 and Y is not
+	start.Y = 300
 
-	arrow := Arrow{}
-	arrow.StartX = 306
-	arrow.StartY = 300
-	arrow.FinishX = 415
-	arrow.FinishY = 23
-	arrow.Width = 2.5
+	arrowLength := 200 // 200 = 2/3 of 1 quadrant of canvas
+	angleRad := (angleDeg - 90) * (math.Pi / 180)
+	fmt.Fprintf(os.Stderr, "angle: %f degrees, %f radians\n", angleDeg, angleRad)
+	finish.X = start.X + int(math.Cos(angleRad)*float64(arrowLength))
+	finish.Y = start.Y + int(math.Sin(angleRad)*float64(arrowLength))
+	arrow.Start = start
+	arrow.Finish = finish
+	arrow.Width = 3.5
 
 	svgArrowBuf := &bytes.Buffer{}
 	err = svgArrowTmpl.Execute(svgArrowBuf, arrow)
 	if err != nil {
-		panic(err)
+		return err
+	}
+	err = svgBaseTmpl.Execute(svgWindroseBuf, svgArrowBuf.String())
+	return nil
+}
+
+/*
+func main() {
+	var (
+		angleDeg float64
+		err      error
+	)
+	if len(os.Args) > 1 {
+		angleDeg, err = strconv.ParseFloat(os.Args[1], 64)
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	err = svgBaseTmpl.Execute(os.Stdout, svgArrowBuf.String())
+	svgWindroseBuf := &bytes.Buffer{}
+	err = GenWindrose(angleDeg, svgWindroseBuf)
+	// err = svgBaseTmpl.Execute(os.Stdout, svgArrowBuf.String())
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("%s\n", svgWindroseBuf.String())
 }
+*/
